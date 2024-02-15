6cf3e9c9669b3218af1787ea68820d0763bfd742
Rabi Krishna Yadav <rabikrishnayadav@gmail.com>
2024-02-15 09:27:14 -0800

    Admin dashboard screen add

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\DashboardController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,4 +17,6 @@ use App\Http\Controllers\AuthController;
 
 Route::get('/', [AuthController::class, 'login']);
 
-Route::get('/forgot', [AuthController::class, 'forgot']);
+Route::get('forgot', [AuthController::class, 'forgot']);
+
+Route::get('admin/dashboard', [DashboardController::class, 'dashboard']);
