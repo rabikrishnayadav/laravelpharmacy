1d1b51fc691ef55deb2c72f9f74bab2fb79a9667
Rabi Krishna Yadav <rabikrishnayadav@gmail.com>
2024-02-15 07:28:32 -0800

    login and forgot screen add

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +14,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [AuthController::class, 'login']);
+
+Route::get('/forgot', [AuthController::class, 'forgot']);
